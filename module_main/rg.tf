d114deb3ccf268d1841ec30324176467e0989f9d
Pierre-Chesne <pierrc@hotmail.com>
2023-03-27 17:26:25 +0200

    first commit

---
+++ module_main/rg.tf
@@ -0,0 +1,7 @@
+resource "azurerm_resource_group" "rg_avd" {
+  name     = "${var.prefix}-rg"
+  location = var.location
+  tags = {
+    infra_avd = var.prefix
+  }
+}
---
+++ module_main/variables.tf
@@ -0,0 +1,9 @@
+variable "prefix" {
+  description = "value of the prefix"
+  type        = string
+}
+
+variable "location" {
+  description = "value of the location"
+  type        = string
+}
