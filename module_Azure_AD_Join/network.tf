0f8902fffa5e096e5c0926b7c221cb4a36a65c88
Pierre-Chesne <pierrc@hotmail.com>
2023-04-17 10:18:15 +0200

    add vnet

--- module_Azure_AD_Join/network.tf
+++ module_Azure_AD_Join/network.tf
@@ -1,7 +1,7 @@
 # terraform azure create virtual network
 resource "azurerm_virtual_network" "vnet_avd" {
   name                = "${var.prefix}-vnet"
-  address_space       = var.vnet_range
+  address_space       = ["10.2.0.0/16"] #var.vnet_range
   location            = azurerm_resource_group.rg_avd.location
   resource_group_name = azurerm_resource_group.rg_avd.name
   tags = {
